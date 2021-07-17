7ec74fd340b0426fde80b81e855f65f56d1a9f5e
Shah rahman <rahman.shah@protonmail.ch>
2021-07-17 12:02:03 +0300

    step2 commit for fast api learning

--- main.py
+++ main.py
@@ -6,9 +6,14 @@ import uvicorn
 app = fastapi.FastAPI()
 
 #step 2 - creating a function which will be called when page is requested
+
 @app.get('/')
 def index():
     return "Hello World!"
 
-#step 3 - to run our API
-uvicorn.run(app)
+#step 3 - to run our API when we want to run locally
+
+if __name__ == '__main__' :
+    uvicorn.run(app)
+
+#step 4 - to run app in prod, use command uviorn maodule:variable which is in our case uvicorn main:app
