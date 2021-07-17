11ea4c7fe75d2deeca41ed4d4eb67b6bc39e6e06
Shah rahman <rahman.shah@protonmail.ch>
2021-07-17 11:49:56 +0300

    step1 commit for fast api learning

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Hello")
+import fastapi
+import uvicorn
+
+# step 1- creating an app
+
+app = fastapi.FastAPI()
+
+#step 2 - creating a function which will be called when page is requested
+@app.get('/')
+def index():
+    return "Hello World!"
+
+#step 3 - to run our API
+uvicorn.run(app)
